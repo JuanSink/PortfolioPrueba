8eeb2c27e11dae53bc0595fa43846fb2eaede54c
JuanSink <juanpablosinkunas08@gmail.com>
2022-10-21 22:42:42 -0300

    Configuraciones en Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyProyectComponent } from './my-proyect/my-proyect.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    MyProyectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +31,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+
+export class AppModule { 
+
+}
---
+++ src/app/my-proyect/my-proyect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-proyect',
+  templateUrl: './my-proyect.component.html',
+  styleUrls: ['./my-proyect.component.scss']
+})
+export class MyProyectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
